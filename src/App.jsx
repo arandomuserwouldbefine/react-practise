472f6b8dd64072953be1379759fc97a8cb8786b5
ayush <ayush@localhost.localdomain>
2024-04-09 11:28:46 +0530

    feat: mui and react boostrap

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,25 @@ import About from './Routing/Pages/about';
 import Contact from './Routing/Pages/contact';
 import Counter from './Routing/Components/counter'; // Corrected import name
 import DummyComponent from './Routing/Components/sasscom';
+import MuiComponents from './Routing/Pages/muiComponents';
+import ReactBoostrap from './Routing/Pages/react-boostrap';
 
 function App() {
   const [count, setCount] = useState(0)
   return (
     <>
       {/* <Counter />  */}
-      <DummyComponent />
-      {/* <Navbar />
+      {/* <DummyComponent /> */}
+      {/* <h1>Hii</h1> */}
+       {/* <Navbar /> */}
 
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path='/mui' element={<MuiComponents />} />
+        <Route path='/rb' element={<ReactBoostrap />} />
+        {/* <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes> */}
+        <Route path="/contact" element={<Contact />} /> */}
+      </Routes> 
     </>
   )
 }
---
+++ src/Routing/Pages/muiComponents.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import BasicSelect from '../Components/mui/select'
+import BasicSwitches from '../Components/mui/switch'
+import FontAwesomeSvgIconDemo from '../Components/mui/icons'
+import DataTable from '../Components/mui/tables'
+import BasicButtons from '../Components/mui/buttons'
+import BasicTextFields from '../Components/mui/forms'
+export default function MuiComponents() {
+    return (
+        <div>
+            {
+                // <BasicSelect />
+                // <BasicSwitches />
+                // <FontAwesomeSvgIconDemo />
+                // <BasicButtons />
+                // <BasicTextFields />
+
+            }
+        </div>
+    )
+}
+
